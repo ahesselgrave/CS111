d3a1e015f5d80d6d346b76cd19b45417c328f69f
Alex Hesselgrave <ahesselgrave@ucla.edu>
2015-10-03 20:32:08 -0700

    Added tokenizing for semicolons; ignores whitespace and passes test.sh

--- lab1/read-command.c
+++ lab1/read-command.c
@@ -1,3 +1,4 @@
+
 // UCLA CS 111 Lab 1 command reading
 
 #include "command.h"
@@ -6,8 +7,10 @@
 #include <error.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 #define TT_BUFFER_SIZE 1024
+#define TT_TOKEN_BUFSIZE 128
 
 // command_stream holds a linked list of command trees, with each node's command_t being the root pointer
 typedef struct command_stream
@@ -16,15 +19,18 @@ typedef struct command_stream
   {
     struct command_node *next;
     command_t command;
-  } *head;
+  } *head, *tail;
 } command_stream;
 
-// Tokens are:
-// ; | && || ( ) < >
-// Comments are:
-// #
-// Treat as regular char:
-// ! % + , - . / : @ ^ _
+typedef char *token;
+typedef struct token_stream
+{
+  struct token_node
+  {
+    token t;
+    struct token_node *next;
+  } *head, *tail;
+} token_stream;
 
 char *
 read_from_input(int (*get_char) (void *),
@@ -39,20 +45,112 @@ read_from_input(int (*get_char) (void *),
     {
       // Put c into the buffer and make sure it wont extend past memory
       buffer[index++] = c;
-      if (index > bufsize) {
-	bufsize += TT_BUFFER_SIZE;
-	buffer = checked_realloc(buffer, bufsize);
-      }
+      if (index > bufsize)
+	{
+	  bufsize += TT_BUFFER_SIZE;
+	  buffer = checked_realloc(buffer, bufsize);
+	}
     }
+  // Don't forget the zero byte at the end
+  buffer[index] = '\0';
   return buffer;
 }
-       
+
+void
+print_tokens(token_stream *ts)
+{
+  struct token_node *tn = ts->head;
+  while (tn->next != NULL)
+    {
+      printf("%s\n", tn->t);
+      tn = tn->next;
+    }
+}
+
+token_stream *
+tokenize_buffer(char *buffer)
+{
+  int token_bufsize = TT_TOKEN_BUFSIZE;
+
+  // Initialize token_stream and set up an initial node that head and tail point to
+  token_stream *ts = checked_malloc(sizeof(token_stream));
+  ts->head = checked_malloc(sizeof(struct token_node));
+  ts->tail = ts->head;
+  
+  token tok_buf = checked_malloc(sizeof(char) * token_bufsize);
+  // Read 2 chars at a time.
+  // Initialize them to the first two characters.
+  // Safe to assume from test cases that there is no 1 char buffer.
+  int first, second, index = 0, tok_index = 0;
+  int linenum = 1;
+  // Words, : ! % + , - . / : @ ^ _
+  // special tokens: ; | && || ( ) < > 
+  
+  do
+    {
+      first = buffer[index++];
+      if (first != ' ' && first != '\t')
+	tok_buf[tok_index++] = first;
+      second = buffer[index];
+
+      switch(second)
+	{
+	case ';':
+	  // end token and put it in da linked list
+	  // disregard memory, leaks aren't checked
+	  tok_buf[tok_index++] = '\0';
+	  ts->tail->t = tok_buf;
+	  ts->tail->next = checked_malloc(sizeof(struct token_node));
+	  ts->tail = ts->tail->next;
+	  ts->tail->next = NULL;
+
+	  // add semicolon token and increment index so we don't
+	  // put semicolon in another token
+	  ts->tail->t = ";";
+	  ts->tail->next = checked_malloc(sizeof(struct token_node));
+	  ts->tail = ts->tail->next;
+	  ts->tail->next = NULL;
+	  index++;
+	  
+	  // allocate a new block for a new token and reset token index
+	  tok_index = 0;
+	  tok_buf = checked_malloc(sizeof(char) * token_bufsize);
+	  break;
+	case '|':
+	  break;
+	case '&':
+      	  break;
+	case '(':
+	  break;
+	case ')':
+	  break;
+	case '<':
+	  break;
+	case '>':
+	  break;
+	case '#':
+	  break;
+	case '\n':
+	  linenum++;
+	  break;
+	default:
+	  break;
+	}
+    }
+  while (first != '\0' && second != '\0');
+  print_tokens(ts);
+  return 0;
+}
+
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		       void *get_next_byte_argument)
 {
   char *buffer = read_from_input(get_next_byte, get_next_byte_argument);
-  printf("%s", buffer);
+  token_stream *t_stream = tokenize_buffer(buffer);
+
+  free(buffer);
+  free(t_stream);
   return 0;
 }
 
