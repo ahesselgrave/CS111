0624977b2c5e453bedf9230ee6c2d8331c558b7f
kwchui <kristen@lnxsrv01.seas.ucla.edu>
2015-10-15 23:35:53 -0700

    implement pipe

--- lab1/execute-command.c
+++ lab1/execute-command.c
@@ -3,12 +3,15 @@
 #include "command.h"
 #include "command-internals.h"
 
+//ADDED INCLUDE DIRECTIVES
 #include <error.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h> 
-/* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  */
+
+#include<stdio.h>
+//ADDED FUNCTION DEFINITIONS
+void execute_pipe(command_t c, int time_travel);
 
 int
 command_status (command_t c)
@@ -19,58 +22,72 @@ command_status (command_t c)
 void
 execute_command (command_t c, int time_travel)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-  //shuts up compiler
 
   //use series of switch statements for each type of command
   switch (c->type){
-    //case SIMPLE_COMMAND:
+  case SIMPLE_COMMAND:
+    printf("simple command\n");
+    break;
     //case AND_COMMAND:
     //case OR_COMMAND:
     //case SEQUENCE_COMMAND:
   case PIPE_COMMAND:
-    execute_pipe(c);
+    printf("pipe command\n");
+    execute_pipe(c,time_travel);
+    break;
     //case SUBSHELL_COMMAND:
-    //default:
-    
+  default:
+    printf("none of the given commands\n");
+    break;
   }
-  c;
-  time_travel;
-  error (1, 0, "command execution not yet implemented");
+
 }
 
-execute_pipe(command_t c){
-  int timetravel=0;
+void execute_pipe(command_t c,int time_travel){
   int childStatus;
-  pid_t pid1, pid2, returnPid;
-  int mypipe[2];
-  if (pipe(mypipe)< 0){
-    //error
+  pid_t pid, returnPid;
+  int infoPipe[2];
+  //pipe:unidirectional communication to pass information
+  //dup2: used to redirect stuff from write into pipe and push info on read into stdin
+  if (pipe(infoPipe)< 0){ //error: can't create pipe
     return;
   }
-  pid1=fork();
+  
+  pid=fork();
+  if (pid < 0){ //fork failed
+    return;
+  }
   //child process
-  if (pid1 == 0){
-    close(mypipe[0]); //close read end bc not used
-    dup2(mypipe[1],1);//redirect stdout to write end of pipe
-    //detect is dup2 suceeds
-    close(mypipe[1]); //close write end of pipe
-    execute_command(c->u.command[0],timetravel); //go through switch to find type
+  else if (pid == 0){
+    close(infoPipe[0]); //close read end bc not used
+    //detect is dup2 succeeds:
+    //create copy of write side of pipe and have stdout point to it
+    if (dup2(infoPipe[1],1) == -1){
+      return;
+    }else{
+      close(infoPipe[1]); //close write end of pipe (1 = write, 0 = read)
+      execute_command(c->u.command[0],time_travel); //go through switch to find type
+      //set status
+      c->status = c->u.command[0]->status;
+    }
   }
   else{//parent process
-    pid2=fork();
-    if(pid2 == 0){
-      close(mypipe[1]);
-      dup2(mypipe[0],0);
+      close(infoPipe[1]);
+      returnPid = waitpid(pid,&childStatus,0); //-1 = wait till child proccess done
+
       //detect if dup2 succeeded
-      close(mypipe[0]);
-      execute_command(c->u.command[1],timetravel);
-    }
+      if (dup2(infoPipe[0],0) == -1){
+	return;
+      }else{
+	close(infoPipe[0]);
+        execute_command(c->u.command[1],time_travel);
+	c->status = c->u.command[1]->status;
+      }
+  }
+   /*    //wait till processes complete
     else{
-      close(mypipe[0]);
-      close(mypipe[1]);
+      close(infoPipe[0]);
+      close(infoPipe[1]);
       returnPid=waitpid(-1,&childStatus,0);//-1 = wait till any child process
       //is done
       if(returnPid == pid1){
@@ -78,9 +95,9 @@ execute_pipe(command_t c){
 	c->status=WEXITSTATUS(childStatus);
       }
       else if (returnPid == pid2){
-	//similar to above but must save c->status as child status of second
-	//status
-      }
+	waitpid(pid2,&childStatus,0);
+	c->status= WEXITSTATUS(childStatus);// need to save c->status as child status of second status
+	}
     }
-  }    
+    }    */
 }
