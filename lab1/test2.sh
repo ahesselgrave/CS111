79a29950ba3d8afc56ebcd3956a6eb19cdd607fa
kwchui <kristen@lnxsrv04.seas.ucla.edu>
2015-10-21 22:45:17 -0700

    added test cases and modified README

--- lab1/test2.sh
+++ lab1/test2.sh
@@ -1,6 +1,9 @@
 (cat) < 3 > 2
-cat </etc/resolv.conf
 ls | wc
+(ls | wc)
+(ls) | wc
+ls | (wc)
+(ls) | (wc)
 cat 1 ; ls
 ls | grep a && false
 ls -al | more
@@ -10,3 +13,7 @@ echo meow > moo2
 (echo meow > moo) | diff moo test1.sh> lawl
 (echo m; cat test1.sh) ; ls -al
 diff a b > f
+(ls | grep a && false) && true
+cat < /etc/passwd | tr a-z A-Z | sort -u || echo sort failed!
+cat < /etc/passwd | tr a-z A-Z | sort -u > out || echo sort failed!
+date > newFile
